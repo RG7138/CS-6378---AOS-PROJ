46e4715ce99f3f6417e783b8f2f0e8033f325514
RG7138 <93302934+RG7138@users.noreply.github.com>
2022-09-21 18:02:20 -0500

    Made changes to-  ParseConfigFileHelperClass

--- src/HelperPackage/ParseConfigFileHelperClass.java
+++ src/HelperPackage/ParseConfigFileHelperClass.java
@@ -6,74 +6,82 @@ import java.io.IOException;
 
 public class ParseConfigFileHelperClass {
 	
-	//Remoce this for part 2 and 3
+	//Remove this for part 2 and 3
 	public static ConfigStructure mapFile;
 	
-	public static ConfigStructure readConfigFile(String name) throws IOException{
-		mapFile = new ConfigStructure();
-		int node_count = 0,next = 0;
-		// Keeps track of current node
-		int curNode = 0;
+	public static ConfigStructure ParseConfigFile(String name) throws IOException{
 		
-		String fileName = System.getProperty("user.dir") + "/" + name;
+		int Counter = 0;
+		int nextConfigInfo = 0;
+		
+		// To Keep track of current node
+		int NodeId = 0;
+		
+		mapFile = new ConfigStructure();
+		
+		String filepath = System.getProperty("user.dir") + "/" + name;
 		
 		String line = null;
+		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			
 			while((line = br.readLine()) != null) {
+				
+				// Ignore comments that is anything after #
 				if(line.length() == 0 || line.startsWith("#"))
 					continue;
-				// Ignore comments and consider only those lines which are not comments
-				String[] config_input;
+				
+				String[] ParsedValues;
+				
 				if(line.contains("#")){
-					String[] config_input_comment = line.split("#.*$"); //Ignore text after # symbol
-					config_input = config_input_comment[0].split("\\s+");
+					//spliting line based on # (sharp symbol)
+					String[] CommentLine = line.split("#.*$"); 
+					
+					//Ignore text after # symbol and splitting on space deliminator
+					ParsedValues = CommentLine[0].split("\\s+");
 				}
 				else {
-					config_input = line.split("\\s+");
+					ParsedValues = line.split("\\s+");
 				}
 
-				if(next == 0 && config_input.length == 6){
-					mapFile.numOfNodes = Integer.parseInt(config_input[0]);
-					mapFile.minPerActive = Integer.parseInt(config_input[1]);
-					mapFile.maxPerActive = Integer.parseInt(config_input[2]);
-					mapFile.minSendDelay = Integer.parseInt(config_input[3]);
-					mapFile.snapshotDelay = Integer.parseInt(config_input[4]);
-					mapFile.maxNumber = Integer.parseInt(config_input[5]);
+				if((nextConfigInfo == 0) && (ParsedValues.length == 6)){
+					mapFile.numOfNodes = Integer.parseInt(ParsedValues[0]);
+					mapFile.minPerActive = Integer.parseInt(ParsedValues[1]);
+					mapFile.maxPerActive = Integer.parseInt(ParsedValues[2]);
+					mapFile.minSendDelay = Integer.parseInt(ParsedValues[3]);
+					mapFile.snapshotDelay = Integer.parseInt(ParsedValues[4]);
+					mapFile.maxNumber = Integer.parseInt(ParsedValues[5]);
 					mapFile.adjMtx = new int[mapFile.numOfNodes][mapFile.numOfNodes];
-					next++;
+					nextConfigInfo++;
 				}
-				else if(next == 1 && node_count < mapFile.numOfNodes)
+				else if((nextConfigInfo == 1) && (Counter < mapFile.numOfNodes))
 				{	
-					if(config_input[1].contains("utdallas.edu")) {
-						mapFile.nodes.add(new NodeStructure(Integer.parseInt(config_input[0]),config_input[1],Integer.parseInt(config_input[2])));
+					if(ParsedValues[1].contains("utdallas.edu")) {
+						mapFile.nodes.add(new NodeStructure(Integer.parseInt(ParsedValues[0]),ParsedValues[1],Integer.parseInt(ParsedValues[2])));
 					}
 					else {
-						mapFile.nodes.add(new NodeStructure(Integer.parseInt(config_input[0]),config_input[1]+".utdallas.edu",Integer.parseInt(config_input[2])));
+						mapFile.nodes.add(new NodeStructure(Integer.parseInt(ParsedValues[0]),ParsedValues[1]+".utdallas.edu",Integer.parseInt(ParsedValues[2])));
 					}
-					node_count++;
-					if(node_count == mapFile.numOfNodes){
-						next = 2;
+					Counter++;
+					if(Counter == mapFile.numOfNodes){
+						nextConfigInfo = 2;
 					}
 				}
-				else if(next == 2) {
-					for(String i : config_input){
-						if(curNode != Integer.parseInt(i)) {
-							mapFile.adjMtx[curNode][Integer.parseInt(i)] = 1;
-							mapFile.adjMtx[Integer.parseInt(i)][curNode] = 1;
+				else if(nextConfigInfo == 2) {
+					for(String i : ParsedValues){
+						if(NodeId != Integer.parseInt(i)) {
+							mapFile.adjMtx[NodeId][Integer.parseInt(i)] = 1;
+							mapFile.adjMtx[Integer.parseInt(i)][NodeId] = 1;
 						}
 					}
-					curNode++;
+					NodeId++;
 				}
 			}
 			br.close();  
 		}
-		catch(FileNotFoundException ex) {
-			System.out.println("Unable to open file '" + fileName + "'");                
-		}
-		catch(IOException ex) {
-			System.out.println("Error reading file '" + fileName + "'");                  
+		catch(Exception e) {
+			System.out.println("Error Occured while parsing: '" + filepath + "'"); 
 		}
 		return mapFile;
 	}
@@ -112,26 +120,5 @@ public class ParseConfigFileHelperClass {
 		}
 	}
 	
-//	public static void main(String[] args) throws IOException {
-//		MapProtocol m = ReadConfigFile.readConfigFile("config1.txt");
-//		
-//		for(Node n : m.nodes) {
-//			System.out.println(n.host + " " + n.nodeId + " " + n.port);
-//		}
-//		System.out.println(m.numOfNodes);
-//		System.out.println(m.minPerActive);
-//		System.out.println(m.maxPerActive);
-//		System.out.println(m.minSendDelay);
-//		System.out.println(m.snapshotDelay);
-//		System.out.println(m.maxNumber);
-//		
-//		for(int i=0;i<m.numOfNodes;i++){
-//			for(int j=0;j<m.numOfNodes;j++){
-//				System.out.print(m.adjMtx[i][j]+"  ");
-//			}
-//			System.out.println();
-//		}
-//
-//	}
 }
 
