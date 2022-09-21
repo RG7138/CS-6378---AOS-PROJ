5286c4c5842860c00bdae7e45c7bf10136da2d3d
Pranay Sadavarte <38144223+PranaySadavarte@users.noreply.github.com>
2022-09-21 18:05:21 -0500

    Update ClientConnectionHelperClass.java

--- src/HelperPackage/ClientConnectionHelperClass.java
+++ src/HelperPackage/ClientConnectionHelperClass.java
@@ -1,11 +1,43 @@
 package HelperPackage;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ClientConnectionHelperClass {
+	
+	private InetAddress addressResolution(String hostname){
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName(hostName);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return address;
+	
+	
+	private SocketClient connection(String address, port)
+		Socket client = null;
+		try {
+			client = new Socket(address,port);		
+			System.out.println("Client Connection Achieved(address,port):"+address.getHostAddress()+" "+port);
+		} catch (IOException e) {
+			System.out.println("Connection Broken");
+			e.printStackTrace();
+			System.exit(1);
+		return client;
+	
+	private ObjectOutputStream oos writing(String client.getOutputStream())
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(client.getOutputStream());
+		} catch (IOException e) {
+					
+			e.printStackTrace();
+		}
+		return oos;
 	
 	//Each node acts as a client to all its neighboring nodes
 	public ClientConnectionHelperClass(ConfigStructure mapObject, int curNode) {
@@ -13,13 +45,16 @@ public class ClientConnectionHelperClass {
 			if(mapObject.adjMtx[curNode][i] == 1){
 				String hostName = mapObject.nodeInfo.get(i).host;
 				int port = mapObject.nodeInfo.get(i).port;
-				InetAddress address = null;
-				try {
-					address = InetAddress.getByName(hostName);
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-					System.exit(1);
-				}
+				
+				
+// 				InetAddress address = null;
+// 				try {
+// 					address = InetAddress.getByName(hostName);
+// 				} catch (UnknownHostException e) {
+// 					e.printStackTrace();
+// 					System.exit(1);
+// 				}
+				
 				
 //				try {
 //					address = InetAddress.getLocalHost();
@@ -28,28 +63,37 @@ public class ClientConnectionHelperClass {
 //					e.printStackTrace();
 //					System.exit(1);
 //				}
-				Socket client = null;
-				try {
-					client = new Socket(address,port);
-					
-					System.out.println("Client Connection Achieved(address,port):"+address.getHostAddress()+" "+port);
-				} catch (IOException e) {
-					System.out.println("Connection Broken");
-					e.printStackTrace();
-					System.exit(1);
+				
+				
+// 				Socket client = null;
+// 				try {
+// 					client = new Socket(address,port);
+// 					System.out.println("Client Connection Achieved(address,port):"+address.getHostAddress()+" "+port);
+// 				} catch (IOException e) {
+// 					System.out.println("Connection Broken");
+// 					e.printStackTrace();
+// 					System.exit(1);
 				}
 				//Send client request to all neighboring nodes
+				
+				InetAddress address = addressResolution(hostname);
+				SocketClient = connection(String address, port);
+			
 				mapObject.channels.put(i, client);
 				mapObject.neighbors.add(i);
-				ObjectOutputStream oos = null;
-				try {
-					oos = new ObjectOutputStream(client.getOutputStream());
-				} catch (IOException e) {
+				ObjectOutputStream oos =  writing(String client.getOutputStream());
+				
+				
+			
+// 				ObjectOutputStream oos = null;
+// 				try {
+// 					oos = new ObjectOutputStream(client.getOutputStream());
+// 				} catch (IOException e) {
 					
-					e.printStackTrace();
-				}
+// 					e.printStackTrace();
+// 				}
 				mapObject.oStream.put(i, oos);	
-			}
+// 			}
 		}
 		System.out.println("Sample");
 	}
