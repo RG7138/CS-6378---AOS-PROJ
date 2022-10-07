546126950736bcf63ba289d9f975c109cce31a3a
Akash Biswal <59276466+aksh-8@users.noreply.github.com>
2022-10-07 18:27:33 -0500

    Laucher 6th oct

--- launcher.sh
+++ launcher.sh
@@ -4,8 +4,19 @@ PROG=Main
 NETID=$2
 PROGRAM_PATH=$(pwd)
 
-rm *.class *.out
-javac *.java
+#rm -rf *.class *.out
+find . -type f -name '*.class' -exec rm -rf {} \;
+find . -type f -name '*.out' -exec rm -rf {} \;
+echo $PROGRAM_PATH
+#exit 0
+javac $PROGRAM_PATH/src/MessagePackage/MessageStructure.java				
+echo MESSAGE HELPER STUFF COMPILED
+#javac $PROGRAM_PATH/src/Main.java
+cd $PROGRAM_PATH/src ; javac Main.java ; cd ..
+echo MAIN COMPILED SUCCESSFULY
+
+
+#find . -type f -name '*.java' -exec javac {} \;
 
 cat $1 | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
 (
@@ -17,9 +28,10 @@ cat $1 | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     	read line 
 		nodeId=$( echo $line | awk '{ print $1 }' )
        	host=$( echo $line | awk '{ print $2 }' )
-		ssh -o StrictHostKeyChecking=no -l "$NETID" "$host" "cd $PROGRAM_PATH;java $PROG $nodeId $1" &
+		ssh -o StrictHostKeyChecking=no -l "$NETID" "$host" "cd $PROGRAM_PATH/src;java $PROG $nodeId $1" &
 		echo Started : $nodeId - $host
     done 
 )
+
 
 
